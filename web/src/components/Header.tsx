08416ede8eac1239320d87586bf4a2f1d0d53040
Guilherme S <guinho.shuto@gmail.com>
2023-01-22 17:25:37 -0400

    mobile aula 3

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -20,7 +20,7 @@ export default function Header(){
             </Dialog.Trigger>
             <Dialog.Portal>
               <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0'/>
-              <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-x-1/2'>
+              <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                 <Dialog.Close className='absolute right-6 top-6'><X size={24} aria-label="Fechar" /></Dialog.Close>
                 <Dialog.Title className='text-3xl leading-tight font-extrabold'> Criar Hábito</Dialog.Title> 
                 <NewHabitForm />
