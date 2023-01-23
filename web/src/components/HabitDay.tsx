492767c96415952999d7f55e791728b09274ed9b
Guilherme S <guinho.shuto@gmail.com>
2023-01-22 22:24:04 -0400

    lista habit day

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -2,9 +2,9 @@ import './Habit.css'
 import clsx from 'clsx'
 import * as Popover from '@radix-ui/react-popover'
 import { ProgressBar } from './ProgressBar'
-import * as Checkbox from '@radix-ui/react-checkbox'
-import { Check } from 'phosphor-react'
 import dayjs from 'dayjs'
+import { useEffect } from 'react'
+import { HabitsList } from './HabitsList'
 
 interface HabitProps{
     date: Date
@@ -17,6 +17,7 @@ export function HabitDay({completed = 0, amount = 0, date}: HabitProps) {
 
     const dayAndMonth = dayjs(date).format('DD/MM')
     const weekDay = dayjs(date).format('dddd')
+
 
     return (
         <Popover.Root>
@@ -37,21 +38,7 @@ export function HabitDay({completed = 0, amount = 0, date}: HabitProps) {
                     <span className='mt-1 font-extrabold leading-tight text-3xl'>{dayAndMonth}</span>
 
                     <ProgressBar progress={73} />
-
-                    <div className='mt-6 flex flex-col gap-3'>
-                        <Checkbox.Root className="flex items-center gap-3 group">
-                            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-300 group-data-[state=checked]:border-green-500">
-                                <Checkbox.Indicator>
-                                    <Check size={20} className="text-white" />
-                                </Checkbox.Indicator>
-                            </div>
-                            <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-700">
-                                Beber água
-                            </span>
-                        </Checkbox.Root>
-
-                    </div>
-
+                    <HabitsList date={date}/>
                     <Popover.Arrow className='fill-zinc-900' width={16} height={8}/>
                 </Popover.Content>
             </Popover.Portal>
